974d64e2d9a722727d2a04728f275b0bfe61b58d
Lorison-Gilles <l.gilles.pereira@avanade.com>
2022-02-11 21:38:52 -0300

    🎨 Styled buttons top header

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,26 @@
 import './menu.css'
-import { FiLink } from 'react-icons/fi'
+import { BsYoutube, BsInstagram } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 export default function Menu() {
   return (
     <div className="menuContainer">
       <div className="menuItem">
-        <button className="menuButton">
-          <FiLink className="menuItemIcon" />
-          Links
-        </button>
+        <a
+          className="socialLink"
+          href="https://www.youtube.com/channel/UCJWCecuSUa9CsNQ4MGJ_g3A"
+        >
+          <BsYoutube className="menuItemIcon" />
+        </a>
+        <a
+          className="socialLink"
+          href="https://www.instagram.com/air400.armorer/"
+        >
+          <BsInstagram className="menuItemIcon" />
+        </a>
+        <Link className="menuButton" to="/links">
+          Meus Links
+        </Link>
       </div>
     </div>
   )
